e5cf2a3bb11c9ca805bf22885b0617f218525ed2
Jay Berg <jaybny@gmail.com>
2014-03-27 22:47:04 -0700

    Remove the now ignored directory "some-directory"xcode

--- xcode/fantasybit.build/Debug/ALL_BUILD.build/Script-30EF6523D88C4CAEAD635E85.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/User/GitHub/fantasybit/xcode -f /Users/User/GitHub/fantasybit/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- xcode/fantasybit.build/Debug/ZERO_CHECK.build/Script-F49C6013BC904FDB9DBFBE86.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/User/GitHub/fantasybit/xcode -f /Users/User/GitHub/fantasybit/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
