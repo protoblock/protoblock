e0bc3eaa5372192fd74fdb959efda7a61a9c2224
Jay Berg <jaybny@gmail.com>
2016-06-11 17:25:44 -0700

    WSSERVER prod

--- Applications/WSServer/main.cpp
+++ Applications/WSServer/main.cpp
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
             QCoreApplication::translate("main", "Debug output [default: off]."));
     parser.addOption(dbgOption);
     QCommandLineOption portOption(QStringList() << "p" << "port",
-            QCoreApplication::translate("main", "Port for echoserver [default: 4000]."),
-            QCoreApplication::translate("main", "port"), QLatin1Literal("4000"));
+            QCoreApplication::translate("main", "Port for echoserver [default: PB_WS_TX_PORT]."),
+            QCoreApplication::translate("main", "port"), QLatin1Literal("PB_WS_TX_PORT"));
     parser.addOption(portOption);
     parser.process(a);
 
     bool debug = parser.isSet(dbgOption);
     int port = parser.value(portOption).toInt();
 
-    Server *server = new Server(port, debug);
+    Server *server = new Server(PB_WS_TX_PORT, debug);
     QObject::connect(server, &Server::closed, &a, &QCoreApplication::quit);
 
-    Server *nameServer = new Server(4001,debug);
+    Server *nameServer = new Server(PB_WS_LITE_AGENT_PORT,debug);
     QObject::connect(nameServer, &Server::closed, &a, &QCoreApplication::quit);
 
     return a.exec();
