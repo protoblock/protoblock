ea5d9739c4283246c925b224590eec03a74443a7
Jay Berg <jaybny@gmail.com>
2016-12-18 13:31:45 -0800

    sort schedule

--- ProRotoQml/Protoblock/mediator.cpp
+++ ProRotoQml/Protoblock/mediator.cpp
@@ -245,14 +245,33 @@ void Mediator::updateWeek() {
 
 
         if ( m_theWeek > 0  && m_theWeek < 17) {
+            std::map<int,std::vector<std::pair<fantasybit::GameInfo,fantasybit::GameStatus_Status>>> sorted;
             fantasybit::WeeklySchedule weekly = mGateway->dataService->GetWeeklySchedule(m_theWeek);
             for ( auto &gi : weekly.games()) {
                 auto status = mGateway->dataService->GetGameStatus(gi.id());
-                if ( status.status() == GameStatus_Status_CLOSED )
-                    m_pWeekClosedScheduleModel->append(new WeeklyScheduleModelItem(gi,status.status(),m_pWeekClosedScheduleModel));
-                else
-                    m_pWeeklyScheduleModel->append(new WeeklyScheduleModelItem(gi,status.status(),m_pWeeklyScheduleModel));
+                std::vector<std::pair<fantasybit::GameInfo,fantasybit::GameStatus_Status>> &vec =
+                        sorted[gi.time()];
+                vec.emplace_back(std::make_pair(gi,status.status()));
             }
+
+            for ( auto tvec : sorted ) {
+                qDebug() << " tvec sorted " << tvec.first;
+                for ( auto p : tvec.second ) {
+                    qDebug() << " p tvec " << p.second << p.first.DebugString().data();
+                    if ( p.second == GameStatus_Status_CLOSED )
+                        m_pWeekClosedScheduleModel->append(new WeeklyScheduleModelItem(p.first,p.second,m_pWeekClosedScheduleModel));
+                    else
+                        m_pWeeklyScheduleModel->append(new WeeklyScheduleModelItem(p.first,p.second,m_pWeeklyScheduleModel));
+                }
+            }
+            qDebug() << " done sorted ";
+
+
+//            if ( status.status() == GameStatus_Status_CLOSED )
+//                m_pWeekClosedScheduleModel->append(new WeeklyScheduleModelItem(gi,status.status(),m_pWeekClosedScheduleModel));
+//            else
+//                m_pWeeklyScheduleModel->append(new WeeklyScheduleModelItem(gi,status.status(),m_pWeeklyScheduleModel));
+
 
             const auto &vgr = mGateway->dataService->GetCurrentWeekGameRosters();
             mPlayerProjModel.updateRosters(vgr,mGateway->dataService);
