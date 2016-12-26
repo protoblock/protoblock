93a3c0898e692abd685972755e59373037a40184
Jay Berg <jaybny@gmail.com>
2016-12-26 14:05:45 -0800

    height test - writenames

--- Applications/fantasybit-2016/PeerNode.cpp
+++ Applications/fantasybit-2016/PeerNode.cpp
@@ -478,8 +478,8 @@ int32_t Node::getLastLocalBlockNum() {
     delete it;
 
 #ifdef STOP_HEIGHT_TEST
-    if (num > 9700 )
-        num = 9700;
+    if (num > 9987 )
+        num = 9987;
     qWarning() << " STOP_HEIGHT_TEST " << num;
 #endif
 
@@ -600,7 +600,7 @@ fc::optional<int32_t> Node::getLastGlobalBlockNum() {
 #endif
 
 #ifdef STOP_HEIGHT_TEST
-    height = 9700;
+    height = 9987;
     qWarning() << "getLastGlobalBlockNum STOP_HEIGHT_TEST" << height;
 #endif
 
