70c2a08d1472aefc77894b6ef491965b17c09a1f
Jay Berg <jaybny@gmail.com>
2018-12-13 12:01:16 -0800

    bitcoinwalletworker

--- src/bitcoinwalletworker.cpp
+++ src/bitcoinwalletworker.cpp
@@ -3,12 +3,14 @@
 //
 //  Protoblock
 //
-//  Created by Jay Berg on 12
+//  Created by Jay Berg on 12/10/2018
 //
 */
 #include "bitcoinwalletworker.h"
 
-BitcoinWalletWorker::BitcoinWalletWorker(QObject *parent) : QObject(parent)
-{
+BitcoinWalletWorker::BitcoinWalletWorker(QObject *parent) : QObject(parent) {
+    timer = new QTimer(this);
+
+
 
 }
--- src/bitcoinwalletworker.h
+++ src/bitcoinwalletworker.h
@@ -3,23 +3,31 @@
 //
 //  Protoblock
 //
-//  Created by Jay Berg on 12
+//  Created by Jay Berg on 12/10/2018
 //
 */
 #ifndef BITCOINWALLETWORKER_H
 #define BITCOINWALLETWORKER_H
 
 #include <QObject>
+#include <QTimer>
 
 class BitcoinWalletWorker : public QObject
 {
     Q_OBJECT
+
+    QTimer * timer;
+
 public:
     explicit BitcoinWalletWorker(QObject *parent = nullptr);
 
 signals:
+    void NameBitcoinBalance(const QString &name, quint64 satoshis);
+
 
 public slots:
+    void onUsingFantasyName(const QString &name, const QString &btcaddress);
+
 };
 
 #endif // BITCOINWALLETWORKER_H
