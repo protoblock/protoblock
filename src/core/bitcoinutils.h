a7c83ef63690a5a41c5a90f3244290f069d38b2a
Jay Berg <jaybny@gmail.com>
2019-04-22 16:07:45 -0700

    more parseRawTx

--- src/core/bitcoinutils.h
+++ src/core/bitcoinutils.h
@@ -30,6 +30,32 @@ const std::string SIGHASH_ALL_4 = "01000000";
 const std::string VERSION_SEGWIT = "02000000";
 const std::string VERSION_NORMAL = "01000000";
 
+struct var_data {
+    std::string size;
+    std::string data;
+};
+
+struct btx_input {
+    std::string rtxid; //64
+    std::string txindex; //8
+    var_data script;
+    std::string sequence; //8
+};
+
+struct btx_output {
+    std::string amount; //16 - reverse hex
+    var_data script;
+};
+
+struct bitcoin_tx {
+    std::string version; //8
+    std::string input_count; // var
+    std::vector<btx_input> inputs;
+    std::string output_count; // var
+    std::vector<btx_output> outputs;
+    std::string locktime; //8 - reverse hex
+};
+
 class BitcoinUtils
 {
 public:
@@ -66,17 +92,20 @@ public:
     static void parseRawTx(const QByteArray &rawtx) {
         QByteArray data;
         QDataStream stream(rawtx);
-//        std::string version;
-        int size = VERSION_NORMAL.size();
-        std::vector<char> version(size+1);
-        size = stream.readRawData(&version[0],size);
-        version[size] = '\0';
+        bitcoin_tx btx;
+        {
+            int size = VERSION_NORMAL.size();
+            std::vector<char> version(size+1);
+            size = stream.readRawData(&version[0],size);
+            version[size] = '\0';
+            btx.version = version.data();
 
-        if ( strcmp (version.data(),VERSION_NORMAL.c_str()) == 0)
-            qDebug() << "jayversion yes VERSION_NORMAL ";
-        else
-            qDebug() << "jayversion no VERSION_NORMAL ";
 
+            if ( strcmp (version.data(),VERSION_NORMAL.c_str()) == 0)
+                qDebug() << "jayversion yes VERSION_NORMAL ";
+            else
+                qDebug() << "jayversion no VERSION_NORMAL ";
+        }
 //        version.resize(11);
 //        stream.readRawData(&version[0],10);
 //        version[10] = '\0';
@@ -89,19 +118,101 @@ public:
             vsz[2] = '\0';
             std::string sz(&vsz[0]);
             pb::from_hex(sz,(char *)&numinputs,sizeof( uint8_t ));
+            btx.input_count = sz;
             qDebug() << sz.data() << "jay size" << numinputs << "size jay";
         }
 
+        std::vector<btx_input> &inputs = btx.inputs;
         for ( int i =0; i < numinputs; i++) {
-            std::vector<char> prevhash_reverse(33);
-            stream.readRawData(&prevhash_reverse[0],32);
-            prevhash_reverse[32] = '\0';
+            btx_input in;
 
+            {
+                std::vector<char> prevhash_reverse(65);
+                stream.readRawData(&prevhash_reverse[0],64);
+                prevhash_reverse[64] = '\0';
+                in.rtxid = prevhash_reverse.data();
+            }
 
+            {
+                std::vector<char> ti(9);
+                stream.readRawData(&ti[0],8);
+                ti[8] = '\0';
+                in.txindex = ti.data();
+            }
 
+            {
+                std::vector<char> vsz(3);
+                stream.readRawData(&vsz[0],2);
+                vsz[2] = '\0';
+                in.script.size = vsz.data();
+
+                uint8_t len;
+                pb::from_hex(in.script.size,(char *)&len,sizeof( uint8_t ));
+
+                len*=2;
+                std::vector<char> sdata(len + 1);
+                stream.readRawData(&sdata[0],len);
+                sdata[len] = '\0';
+                in.script.data = sdata.data();
+            }
+
+            {
+                std::vector<char> seq(9);
+                stream.readRawData(&seq[0],8);
+                seq[8] = '\0';
+                in.sequence = seq.data();
+            }
+
+            inputs.push_back(in);
         }
 
+        uint8_t numout = 0;
+        {
+            std::vector<char> vsz(3);
+            stream.readRawData(&vsz[0],2);
+            vsz[2] = '\0';
+            //std::string sz(&vsz[0]);
+            pb::from_hex(&vsz[0],(char *)&numout,sizeof( uint8_t ));
+            btx.output_count = vsz.data();
+        }
 
+        std::vector<btx_output> &outs = btx.outputs;
+        for ( int i =0; i < numout; i++) {
+            btx_output out;
+
+            {
+                std::vector<char> amount(17);
+                stream.readRawData(&amount[0],16);
+                amount[16] = '\0';
+                out.amount = amount.data();
+            }
+
+
+            {
+                std::vector<char> vsz(3);
+                stream.readRawData(&vsz[0],2);
+                vsz[2] = '\0';
+                out.script.size = vsz.data();
+
+                uint8_t len;
+                pb::from_hex(out.script.size,(char *)&len,sizeof( uint8_t ));
+
+                len*=2;
+                std::vector<char> sdata(len + 1);
+                stream.readRawData(&sdata[0],len);
+                sdata[len] = '\0';
+                out.script.data = sdata.data();
+            }
+
+            outs.push_back(out);
+        }
+
+        {
+            std::vector<char> lock(9);
+            stream.readRawData(&lock[0],8);
+            lock[8] = '\0';
+            btx.locktime = lock.data();
+        }
     }
 
     /*
@@ -111,11 +222,30 @@ public:
     0123449fad6289dda5365a197fa822e320cdfc106bed243bf773cac64cfdb237
     05000000
     6a
-    1333183ddf384da83ed49296136c70d206ad2b19331bf25d390e69b222165e37
-    473044022036be6403aeb4e0e6fd54720b328d9d81bea32fb79684da0288743668fb5ef3ee02202023a71ef7217061fb9b4f35a05143de71447032e5a35b39c3d14b3210bad10b0121032725846bb7bc2e47b7b5a50670d77c8268f4d7f3243bdcf1b22174a67faaf528
+        47
+        3044022036be6403aeb4e0e6fd54720b328d9d81bea32fb79684da0288743668fb5ef3ee02202023a71ef7217061fb9b4f35a05143de71447032e5a35b39c3d14b3210bad10b01
+        21
+        032725846bb7bc2e47b7b5a50670d77c8268f4d7f3243bdcf1b22174a67faaf528
     feffffff
-    0200213900000000001976a914659042e01e864e2f29641ea3a213c51a956d33c788ac288c0f00000000001976a9144fc238bcda3f884ff6ce8d9feeb89b50dfd3da8888ac2c480700
- *
+    02  // num outs
+    0021390000000000 // amount
+    19               // len
+        76a9
+        14
+        659042e01e864e2f29641ea3a213c51a956d33c7
+        88ac
+    288c0f0000000000  / amount
+    19
+        76a9
+        14
+        4fc238bcda3f884ff6ce8d9feeb89b50dfd3da88
+        88ac
+    2c480700  //locktime
+
+
+
+
+
     const MyKey32 Transaction::getDigest ( const int n, const QByteArray& scr ) const
     {
       MyByteArray data;                                 // create empty array
